f0428996a256bb88700ce4e52b8c5a4caa9abf41
Nadine Duursma <n.a.duursma@outlook.com>
2021-10-23 17:04:27 +0200

    Add code

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# The-Ulitmate-Get-data-from-NIST-without-interpolation-Repository
+# The Ulitmate 'Get data from NIST/JANAF without interpolation' Repository
+
+This repository contains the code to obtain thermochemical data from NIST and JANAF. The Python code does the database searching and interpolation, so you do not need to do it yourself anymore :).
+
+## Installation
+
+Clone this repository.
+```bash
+ git clone -c core.longpaths=true https://github.com/nduursma/The-Ulitmate-Get-data-from-NIST-without-interpolation-Repository
+```
+Change your working directory.
+```bash
+cd The-Ulitmate-Get-data-from-NIST-without-interpolation-Repository
+```
+
+Create a new environment and activate it.
+```bash
+conda env create -f TRP.yml
+conda activate TRP
+```
+
+Open the file "get_data.py", run the following command if you prefer to use Spyder.
+```bash
+spyder get_data.py
+```
+
+Enter your temperature, the molecular formula, and the phase in the code.
---
+++ get_data.py
@@ -0,0 +1,66 @@
+"""
+Created on Sat Oct 23 15:02:26 2021
+
+@author: nadin
+"""
+from tabulate import tabulate
+
+# =================================================================================================================
+# Enter your data here
+# =================================================================================================================
+
+T = 3500;                # Reaction temperature [K]
+formula_ = 'H2O'         # Molecule, find by running the file and printing the options
+phase_ = 'g'             # Phase, choose from 'cr', 'l', 'cr,l', 'g', 'ref', 'cd', 'fl', 'am', 'vit', 'mon', 'pol', 'sln', 'aq', 'sat'
+show_options = True      # Print the formulas in the data base to choose from
+
+# ================================================================================================================= 
+# Constants
+# ================================================================================================================= 
+ 
+R = 8.314472            #  Universal gas constant R 
+Tr = 298.15             # Reference temp [K]
+
+# ================================================================================================================= 
+# Code
+# ================================================================================================================= 
+ 
+if 'db' not in globals():
+    from thermochem.janaf import Janafdb
+    db = Janafdb()
+
+
+if show_options == True:
+    options = db.search(formula_)
+    print(options)
+
+
+p = db.getphasedata(formula=formula_, phase=phase_)
+# p = db.getphasedata(formula=formula_, name = name_, phase=phase_)
+
+# ================================================================================================================= 
+# Print the results
+# ================================================================================================================= 
+
+print('\n ======================================================================================')
+print('\n ---------------------- Properties of', formula_, 'at', T, '[K] ---------------------------------')
+print('\n ====================================================================================== \n')
+
+l1 = ['Enthalpy','H(T)-H(Tr)', p.hef(T), '[kJ/mol]']
+l2 = ['Heat capacity','C_p', p.cp(T), '[J/mol/K]']
+l3 = ['Enthalpy of formation','DeltaH', p.DeltaH(Tr), '[kJ/mol]']
+l4 = ['Equilibrium constant', 'K_p', 10**(p.logKf(T)), '[-]']
+
+L = [l1, l2, l3, l4]
+headers_ = ['Property', 'Symbol', 'Value', 'Unit']
+
+print(tabulate(L, headers=headers_))
+
+
+
+
+
+
+
+
+
